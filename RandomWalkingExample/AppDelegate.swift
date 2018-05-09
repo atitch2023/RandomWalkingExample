1c155ad72f895106b35b0cfb6763972cdd7ff94c
Arlan Titchbourne <nalra@arlans-macbook-pro.local>
2018-05-08 20:10:09 -0600

    Initial Commit

---
+++ RandomWalkingExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RandomWalkingExample
+//
+//  Created by Arlan Titchbourne on 5/8/18.
+//  Copyright © 2018 Arlan Titchbourne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RandomWalkingExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RandomWalkingExample
+//
+//  Created by Arlan Titchbourne on 5/8/18.
+//  Copyright © 2018 Arlan Titchbourne. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
